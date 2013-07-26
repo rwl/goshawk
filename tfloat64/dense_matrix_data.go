3cbea540486e84c49d25aa8d94c815d88ffe79ab
rwl <r.w.lincoln@gmail.com>
2013-07-26 19:41:27 +0200

    Fixing row stride for row major order matrix.

--- tfloat64/dense_matrix_data.go
+++ tfloat64/dense_matrix_data.go
@@ -6,7 +6,7 @@ import "bitbucket.org/rwl/colt"
 func NewMatrix(rows, columns int) *Matrix {
 	return &Matrix{
 		DenseMatrixData{
-			colt.NewCoreMatrixData(false, rows, columns, 1, 1, 0, 0),
+			colt.NewCoreMatrixData(false, rows, columns, columns, 1, 0, 0),
 			make([]float64, rows*columns),
 		},
 	}
@@ -27,4 +27,8 @@ func (m DenseMatrixData) SetQuick(row, column int, value float64) {
 
 func (m DenseMatrixData) Elements() interface{} {
 	return m.elements
+}
+
+func (m DenseMatrixData) Index(row, column int) int {
+	return m.RowZero() + row*m.RowStride() + m.ColumnZero() + column*m.ColumnStride()
 }
