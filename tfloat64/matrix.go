f87d4fbd926f86635479033b0f652f8fcc3512ee
rwl <r.w.lincoln@gmail.com>
2013-07-28 16:59:15 +0200

    Implementing remaining matrix aggregation tests.

--- tfloat64/matrix.go
+++ tfloat64/matrix.go
@@ -1,8 +1,51 @@
 
 package tfloat64
 
+import "fmt"
+
 type Matrix struct {
 	MatrixData
+}
+
+func (m *Matrix) checkShape(other MatrixData) error {
+	if m.Rows() != other.Rows() || m.Columns() != other.Columns() {
+		return fmt.Errorf("row sizes do not match: %d!=%d", m.Rows(), other.Rows())
+	}
+	if m.Columns() != other.Columns() {
+		return fmt.Errorf("column sizes do not match: %d!=%d", m.Columns(), other.Columns())
+	}
+	return nil
+}
+
+func (m *Matrix) checkColumn(column int) error {
+	if column < 0 || column >= m.Columns() {
+		return fmt.Errorf("Attempted to access %s at column=%d", m.StringShort(), column)
+	}
+	return nil
+}
+
+func (m *Matrix) checkBox(row, column, height, width int) error {
+	if column < 0 || width < 0 || column + width > m.Columns() || row < 0 || height < 0 || row + height > m.Rows() {
+		return fmt.Errorf("%s, column:%d, row:%d, width:%d, height:%d", m.StringShort(), column, row, width, height)
+	}
+	return nil
+}
+
+func (m *Matrix) checkRow(row int) error {
+	if row < 0 || row >= m.Rows() {
+		return fmt.Errorf("Attempted to access %s at row=%d", m.StringShort(), row)
+	}
+	return nil
+}
+
+// Returns a string representation using default formatting.
+func (m *Matrix) String() string {
+	return fmtr.MatrixToString(m)
+}
+
+// Returns a short string representation of the receiver's shape.
+func (m *Matrix) StringShort() string {
+	return fmtr.MatrixShape(m)
 }
 
 // Returns the number of cells which is Rows()*Columns().
--- tfloat64/matrix_aggregate.go
+++ tfloat64/matrix_aggregate.go
@@ -17,3 +17,57 @@ func (m *Matrix) Aggregate(aggr Float64Float64Func, f Float64Func) float64 {
 	}
 	return a
 }
+
+func (m *Matrix) AggregateProcedure(aggr Float64Float64Func, f Float64Func, cond Float64Procedure) float64 {
+	if m.Size() == 0 {
+		return math.NaN()
+	}
+	a := 0.0
+	elem := m.GetQuick(0, 0)
+	if cond(elem) {
+		a = aggr(a, f(elem))
+	}
+	d := 1 // First cell already done.
+	for r := 0; r < m.Rows(); r++ {
+		for c := d; c < m.Columns(); c++ {
+			elem = m.GetQuick(r, c)
+			if cond(elem) {
+				a = aggr(a, f(elem))
+			}
+		}
+		d = 0
+	}
+	return a
+}
+
+func (m *Matrix) AggregateProcedureSelection(aggr Float64Float64Func, f Float64Func, rowList, columnList []int) float64 {
+	if m.Size() == 0 {
+		return math.NaN()
+	}
+	var elem float64
+	a := f(m.GetQuick(rowList[0], columnList[0]))
+	for i := 1; i < len(rowList); i++ {
+		elem = m.GetQuick(rowList[i], columnList[i])
+		a = aggr(a, f(elem))
+	}
+	return a
+}
+
+func (m *Matrix) AggregateMatrix(other MatrixData, aggr Float64Float64Func, f Float64Float64Func) (float64, error) {
+	err := m.checkShape(other)
+	if err != nil {
+		return math.NaN(), err
+	}
+	if m.Size() == 0 {
+		return math.NaN(), nil
+	}
+	a := f(m.GetQuick(0, 0), other.GetQuick(0, 0))
+	d := 1 // First cell already done.
+	for r := 0; r < m.Rows(); r++ {
+		for c := d; c < m.Columns(); c++ {
+			a = aggr(a, f(m.GetQuick(r, c), other.GetQuick(r, c)))
+		}
+		d = 0
+	}
+	return a, nil
+}
